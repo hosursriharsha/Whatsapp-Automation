8d71a280c220a4420e9255548b3c35f45372ae21
hosursriharsha <hosursriharsha@gmail.com>
2018-09-10 22:21:06 +0530

    Modified code to handle two device scenarios

--- Stepdefs/com/whatsapptest/Chats/VerifySearchContactAndSendMsg.java
+++ Stepdefs/com/whatsapptest/Chats/VerifySearchContactAndSendMsg.java
@@ -2,28 +2,32 @@ package com.whatsapptest.Chats;
 
 import static org.testng.Assert.assertTrue;
 import com.whatsapptest.base.BaseClass;
+
+import Pages.HomePage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class VerifySearchContactAndSendMsg extends BaseClass {
+public class VerifySearchContactAndSendMsg {
 
-  @Given("^\"([^\"]*)\" is logged into whatsapp$")
-  public void verifyLoginWhatsapp(String User1) throws Throwable {
-    assertTrue(
-        driver(User1).findElementByXPath(getLocator("chatsScreen.ChatsLabel")).isDisplayed());
-  }
+	private HomePage homePage = new HomePage();
 
-  @When("^\"([^\"]*)\" searches for \"([^\"]*)\"$")
-  public void searches_for(String user1, String user2) throws Throwable {
-    driver(user1).findElementById(getLocator("chatsScreen.SearchContact")).click();
-    driver(user1).findElementById(getLocator("chatScreen.SearchContactTextBox"))
-        .sendKeys(getDeviceValue(user1, "userName"));
-  }
+	@Given("^\"([^\"]*)\" is logged into whatsapp$")
+	public void verifyHomePage(String User1) throws Throwable {
+		homePage.verifyHomePage(User1);
+	}
 
-  @Then("^\"([^\"]*)\" chooses the contact \"([^\"]*)\"$")
-  public void chooses_the_contact(String arg1, String arg2) throws Throwable {}
+	@When("^\"([^\"]*)\" searches for \"([^\"]*)\"$")
+	public void searchForContact(String User1, String User2) throws Throwable {
+		homePage.searchContact(User1, User2);
+	}
 
-  @Then("^sends the message \"([^\"]*)\"$")
-  public void sends_the_message(String arg1) throws Throwable {}
+	@Then("^\"([^\"]*)\" chooses the contact \"([^\"]*)\"$")
+	public void chooseContact(String User1, String User2) throws Throwable {
+		homePage.chooseContact(User1,User2);
+	}
+
+	@Then("^sends the message \"([^\"]*)\"$")
+	public void sends_the_message(String User1) throws Throwable {
+	}
 }
